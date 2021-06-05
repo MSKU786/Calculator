698e2e6388a5a835c323cf54c0aff65c889402d7
MANISH SINGH <manishsingh332000@gmail.com>
2021-06-05 10:03:40 +0530

    Almost done

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
 
 var buttons = document.getElementsByClassName("number");
 var display = document.getElementById("display");
+var previous = document.getElementById("prevDisplay");
+var operation = document.getElementById("operation");
 var operator = null;
 var operand1 = 0;
 var operand2 = 0;
-console.log('yes');
+
 for(var i=0;i<buttons.length;i++)
 {
 	buttons[i].addEventListener('click', function(){
@@ -21,10 +23,23 @@ for(var i=0;i<buttons.length;i++)
 					  }
 					  else
 					  	   operand1 = parseFloat(display.textContent);
+					   prevDisplay.innerText = prevDisplay.innerText+ operation.innerText+display.innerText ;
+					   operation.innerText = "+"
 					   display.innerText = "";
 					   break;
 			case '-': operator = '-';
-					  break;
+						if(operand1!=null)
+						{
+								var temp = parseFloat(display.textContent);
+								operand1 = eval(temp+operator+operand1);
+						}
+						else
+							operand1 = parseFloat(display.textContent);
+							prevDisplay.innerText = prevDisplay.innerText+ operation.innerText+display.innerText ;
+						operation.innerText = "-"
+						display.innerText = "";
+						break;
+					  
 			case '*': operator = '*';
 					  if(operand1!=null)
 					  {
@@ -33,6 +48,9 @@ for(var i=0;i<buttons.length;i++)
 					  }
 					  else
 					  	   operand1 = parseFloat(display.textContent);
+							 prevDisplay.innerText = prevDisplay.innerText+ operation.innerText+display.innerText ;
+						operation.innerText = "*"
+						display.innerText = "";
 					   break;
 
 			case '%': operator = '%';
@@ -43,6 +61,9 @@ for(var i=0;i<buttons.length;i++)
 					  }
 					  else
 					  	   operand1 = parseFloat(display.textContent);
+							 prevDisplay.innerText = prevDisplay.innerText+ operation.innerText+display.innerText ;
+						operation.innerText = "%"
+						display.innerText = "";
 					   break;
 			case '/': operator = '/';
 					  if(operand1!=null)
@@ -52,24 +73,31 @@ for(var i=0;i<buttons.length;i++)
 					  }
 					  else
 					  	   operand1 = parseFloat(display.textContent);
+							 prevDisplay.innerText = prevDisplay.innerText+ operation.innerText+display.innerText ;
+					   operation.innerText = "/"
 					   display.innerText = "";
 					   break;
 			case "=" : operand2 = parseFloat(display.textContent);
 					   var ans = eval(operand1+ operator+operand2);
+					   prevDisplay.innerText = ans ;
 					   display.innerText=ans;
 					   break;
 			case "AC" : operand1 = null;
 						operand2 = null;
 						operator = "";
+						prevDisplay.innerText = "";
+						operation.innerText = "";
 						display.innerText = "";
 						break;
 			case "+/-" :operand1 = parseFloat(display.textContent);
 						operand1 = eval("-1"+"*"+parseFloat(operand1));
+						operation.innerText = "-"
 						console.log(operand1);
 						display.innerText = operand1;
 						break;
 			case "square" :operand1 = parseFloat(display.textContent);
 						   operand1 = Math.pow(operand1,2);
+						   operation.innerText = "^2"
 						   console.log(operand1);
 						   display.innerText = operand1;
 						   break;
